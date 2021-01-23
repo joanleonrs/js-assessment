858165506644f68b5d1f7da34063a8b715e4309b
Jose Leon <joanleonrs@gmail.com>
2021-01-23 13:00:24 -0500

    bugfix(strings): improve implementation into string functions

--- app/strings.js
+++ app/strings.js
@@ -3,10 +3,47 @@ exports = typeof window === 'undefined' ? global : window;
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
 
+    var stringResult = '';
+    var inputStringArr = str.split('');
+    var charItem;
+    var charCount = {};
+
+    for (var i = 0, len = inputStringArr.length; i < len; i++) {
+
+      charItem = inputStringArr[i];
+      charCount[charItem] = typeof charCount[charItem] === 'undefined' ? 1 : charCount[charItem] + 1;
+
+      if (charCount[charItem] <= amount) {
+        stringResult += charItem;
+      }
+    }
+
+    return stringResult;
   },
 
   wordWrap: function(str, cols) {
+    var words = str.split(' ');
+    var stringResult = '';
+    var currentLength = 0;
+    var item;
+    var token;
 
+    for (var i = 0, lgth = words.length; i < lgth; i++) {
+      item = words[i];
+      currentLength += item.length;
+
+      if (i === 0) {
+        token = '';
+      } else if (currentLength > cols) {
+        currentLength = item.length;
+        token = '\n';
+      } else {
+        token = ' ';
+      }
+      stringResult+= token +item;
+    }
+
+    return stringResult;
   },
 
   reverseString: function(str) {
