aed3c5fbf3672d95916c8402c7e088c1519afe27
Jose Leon <joanleonrs@gmail.com>
2021-01-23 12:33:34 -0500

    bugfix(async): fixes into async functions

--- app/async.js
+++ app/async.js
@@ -1,11 +1,29 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
 
+  async: function(value) {
+    var deferred = $.Deferred();
+    setTimeout(function() {
+      deferred.resolve(value);
+    }, 5);
+
+    return deferred.promise();
   },
 
   manipulateRemoteData: function(url) {
+    var deferred = $.Deferred();
 
+    $.ajax(url).then(function(response) {
+      var someone = $.map(
+        response.people, function(people) {
+          return people.name;
+        }
+      );
+      // deferred.resolve(someone);
+      deferred.resolve(someone.sort());
+    });
+
+    return deferred.promise();
   }
 };
