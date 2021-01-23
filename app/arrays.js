b1fbba89e7e1b623e796a78291d1562e957339de
Jose Leon <joanleonrs@gmail.com>
2021-01-23 11:01:01 -0500

    bugfix(arrays): fixes on array methods part 1

--- app/arrays.js
+++ app/arrays.js
@@ -2,42 +2,50 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+
     return arr.indexOf(item);
   },
 
   sum: function(arr) {
+
+    return arr.reduce((a,b) => a + b, 0);
   },
 
   remove: function(arr, item) {
-
+    return arr.filter(value => value === item);
   },
 
   removeWithoutCopy: function(arr, item) {
-
+    arr.filter(value => value === item);
+    return arr;
   },
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
-
+    arr.pop()
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail: function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat: function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert: function(arr, item, index) {
-
+    return arr.splice(item, index);
   },
 
   count: function(arr, item) {
